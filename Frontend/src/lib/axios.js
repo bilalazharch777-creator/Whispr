3917dcda2c6664748a06ed32f243b3ac3a83fc4b
Billy <bilalazharch777@gmail.com>
2026-05-05 13:24:29 +0500

    trigger redeploy

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
   withCredentials: true,
