214466427582f43e11dd1d175e1bfe674e3311cc
Billy <bilalazharch777@gmail.com>
2026-05-05 13:22:11 +0500

    trigger redeploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,35 @@
-import express from 'express';
+import express from "express";
 
 import "dotenv/config";
-import authRoutes from "./Routes/auth.route.js"
-import userRoutes from "./Routes/user.route.js"
-import chatRoutes from "./Routes/chat.route.js"
-import postRoutes from "./Routes/post.route.js"
-import storyRoutes from "./Routes/story.route.js"
-import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-
+import authRoutes from "./Routes/auth.route.js";
+import userRoutes from "./Routes/user.route.js";
+import chatRoutes from "./Routes/chat.route.js";
+import postRoutes from "./Routes/post.route.js";
+import storyRoutes from "./Routes/story.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.use("/public", express.static("public")); // for avatars
 
-app.use(cors({
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://whispr-z55k.vercel.app"],
     credentials: true,
-}));
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth/", authRoutes);  
+app.use("/api/auth/", authRoutes);
 app.use("/api/users/", userRoutes);
 app.use("/api/chat/", chatRoutes);
 app.use("/api/post/", postRoutes);
 app.use("/api/story/", storyRoutes);
 
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server is Running on Port ${PORT}!`);
-    connectDB();
-       
-})
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}!`);
+  connectDB();
+});
