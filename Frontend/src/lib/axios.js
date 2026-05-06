48cfbbc2f138ffb6f33d3a0e6bf6f548c7d935ee
Billy <bilalazharch777@gmail.com>
2026-05-06 15:40:59 +0500

    added render URL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
-  // TODO: removed temporarily
-  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
-  baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
   withCredentials: true,
 });
